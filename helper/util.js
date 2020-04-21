f4e638bafd3ec11cda11969da562ce278680d835
Alfian Maulana <alfianm.ibrahim@gmail.com>
2020-04-21 11:17:49 +0700

    Added /real-time and helper functions

--- helper/util.js
+++ helper/util.js
@@ -1,19 +1,29 @@
 var pool = require('../db/config')
+var format = 'dd-mm-yyyy'
 
-exports.getSensorData = async (recipient) => {
+exports.getSensorData = async (landId, timeStart, timeEnd) => {
   const client = await pool.connect()
-  const result = await client.query('SELECT * FROM pengguna;')
+  const result = await client.query(`SELECT * FROM data_sensor WHERE id_lahan = $1 AND waktu >= to_timestamp($2, $4)
+                                     AND waktu <= to_timestamp($3, $4);`, [landId, timeStart, timeEnd, format])
   client.release()
   return result
 }
 
 exports.calculateAverage = async (data) => {
+
+}
+
+exports.compareTime = async (time1, time2) => {
   const client = await pool.connect()
-  const result = await client.query('SELECT * FROM pengguna;')
+  const result = await client.query('SELECT to_timestamp($1, $3) > to_timestamp($2, $3);', [time1, time2, format])
   client.release()
   return result
 }
 
-exports.timeParser = async (time) => {
-
+exports.createTime = (time) => {
+  const timeObject = new Date(time)
+  const date = ('0' + timeObject.getDate()).slice(-2)
+  const month = ('0' + (timeObject.getMonth() + 1)).slice(-2)
+  const year = timeObject.getFullYear()
+  return date + '-' + month + '-' + year
 }
--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,84 @@
 var express = require('express')
 var router = express.Router()
 var Util = require('../helper/util')
+var ML = require('../helper/ml')
 
 /*
  * Get data for real-time monitoring
+ * Constraint: Min timeStart = lahan(created_at), Max timeEnd = today, format = dd-mm-yyyy
  */
-router.get('/real-time/:id/:land/:timeStart/:timeEnd', (req, res) => {
-  // timeEnd max today
-  Util.timeParser()
-  Util.getSensorData()
-  Util.calculateAverage()
-  res.send('real-time')
+router.get('/real-time/:landId/:timeStart?/:timeEnd?', async (req, res) => {
+  try {
+    if (!req.params.landId) {
+      return res.status(400).send({ status: 400, message: 'No landId provided' })
+    }
+    const timeStart = req.params.timeStart ? req.params.timeStart : Util.createTime(Date.now())
+    const timeEnd = req.params.timeEnd ? req.params.timeEnd : Util.createTime(Date.now())
+    const isInvalidTime = (await Util.compareTime(timeStart, timeEnd)).rows[0]['?column?']
+    if (isInvalidTime) {
+      return res.status(400).send({ status: 400, message: 'Time start must not exceed time end' })
+    }
+    const responseData = await Util.getSensorData(req.params.landId, timeStart, timeEnd)
+    const data = responseData ? responseData.rows : null
+    if (!data || data.length === 0) {
+      return res.status(400).send({ status: 400, message: 'Data is empty' })
+    }
+    const responseAverage = await Util.calculateAverage(data)
+    const average = responseAverage || null
+    const results = {
+      status: 'Successfully get real-time data',
+      data: {
+        average: average,
+        sensor_data: data
+      }
+    }
+    res.send(results)
+  } catch (err) {
+    console.error(err)
+    return res.status(500).send(err)
+  }
 })
 
 /*
  * Get data for prediction of land conditions
+ * Constraint: Min time = today, Max time = 5 years, time is dd-mm-yyyy
  */
-router.get('/prediction/:id/:land/:timeStart/:timeEnd', (req, res) => {
-  // timeStart min today, timeEnd max 5 years
-  Util.timeParser()
-  Util.getSensorData()
-  // request.post getPredictionML()
-  res.send('prediction')
+router.get('/prediction/:landId/:time', (req, res) => {
+  try {
+    if (!req.params.landId || !req.params.time) {
+      return res.status(400).send({ status: 400, message: 'No landId or time provided' })
+    }
+    // Util.timeParser()
+    const time = req.params.time ? req.params.time : Date.now()
+    const responseData = Util.getSensorData(req.params.landId, time)
+    const data = responseData ? responseData.rows : null
+    if (!data) {
+      return res.status(400).send({ status: 400, message: 'Data is empty' })
+    }
+    const predictedData = ML.predict(data)
+    const responseAverage = Util.calculateAverage(predictedData)
+    const average = responseAverage || null
+    // request.post getPredictionML()
+    const results = {
+      status: 'Successfully get predicted data',
+      data: {
+        average: average,
+        predicted_data: predictedData
+      }
+    }
+    res.send(results)
+  } catch (err) {
+    console.error(err)
+    return res.status(500).send(err)
+  }
 })
 
 /*
  * Get recommendation for certain land
  */
-router.get('/recommendation/:id/:land', (req, res) => {
+router.get('/recommendation/:landId', (req, res) => {
   Util.getSensorData()
+  ML.recommend()
   // request.post getRecommendationML()
   res.send('recommendation')
 })
