a886d753d9d542e5c8c3359088e7e957ce3203f6
Alfian Maulana <alfianm.ibrahim@gmail.com>
2020-04-21 11:17:31 +0700

    Install dotenv

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 var createError = require('http-errors')
 var express = require('express')
 var path = require('path')
@@ -5,7 +7,6 @@ var cookieParser = require('cookie-parser')
 var logger = require('morgan')
 
 var indexRouter = require('./routes/index')
-var usersRouter = require('./routes/users')
 
 var app = express()
 
@@ -16,7 +17,6 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
-app.use('/users', usersRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
